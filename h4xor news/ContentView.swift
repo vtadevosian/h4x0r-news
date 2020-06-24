67e8daff00f3770b5a901c1554e0ffba41feff06
vtadevosian <vahram_tadevosyan@edu.aua.am>
2020-06-24 15:26:01 +0400

    Initial Commit

---
+++ h4xor news/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  h4xor news
+//
+//  Created by Vahram Tadevosian on 6/24/20.
+//  Copyright © 2020 Vahram Tadevosian. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
